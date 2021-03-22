ec6ea61d9605e292ee6f045b2b41d6c95d41cc9d
Ivailo Spirdonov <78851544+ivailospirdonov@users.noreply.github.com>
2021-03-22 13:56:02 +0200

    Added Sign Up page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Signup from './components/authComponents/Signup';
+import { Container } from 'react-bootstrap';
 
 
 function App() {
   return (
     <div className="site-container">
       <Header />
-
-        <Signup />
+        <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "90vh" }}>
+          <div className="w-100" style={{ maxWidth: "400px" }}>
+            <Signup />
+          </div>
+        </Container>
         
       <Footer />
     </div>
