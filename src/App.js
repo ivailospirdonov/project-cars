7fee534f91fba9c2154a220ec9146f4e4d7d2e9f
Ivailo Spirdonov <78851544+ivailospirdonov@users.noreply.github.com>
2021-03-22 18:34:54 +0200

    Finished Sign up page and authentication context

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Signup from './components/authComponents/Signup';
 import { Container } from 'react-bootstrap';
+import { AuthProvider } from './contexts/AuthContext';
 
 
 function App() {
   return (
-    <div className="site-container">
-      <Header />
+    <AuthProvider>
+      <div className="site-container">
+        <Header />
         <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "90vh" }}>
           <div className="w-100" style={{ maxWidth: "400px" }}>
             <Signup />
           </div>
         </Container>
-        
-      <Footer />
-    </div>
+
+        <Footer />
+      </div>
+    </AuthProvider>
   );
 }
 
