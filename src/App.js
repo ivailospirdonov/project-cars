333fdceb517ff4e8153c753222155bb69452b885
Ivailo Spirdonov <78851544+ivailospirdonov@users.noreply.github.com>
2021-03-23 17:32:33 +0200

    added router and log in page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Dashboard from './components/homePageComponents/Dashboard';
 import Signup from './components/authComponents/Signup';
+import Login from './components/authComponents/Login';
 import { Container } from 'react-bootstrap';
 import { AuthProvider } from './contexts/AuthContext';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 
 function App() {
@@ -12,7 +15,13 @@ function App() {
         <Header />
         <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "90vh" }}>
           <div className="w-100" style={{ maxWidth: "400px" }}>
-            <Signup />
+            <Router>
+              <Switch>
+                <Route exact path="/" component={Dashboard} />
+                <Route path="/signup" component={Signup} />
+                <Route path="/login" component={Login} />
+              </Switch>
+            </Router>
           </div>
         </Container>
 
