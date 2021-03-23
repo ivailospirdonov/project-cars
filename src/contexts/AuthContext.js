2e28cd2523977762001c2527df7fdd1374f0ef7a
Ivailo Spirdonov <78851544+ivailospirdonov@users.noreply.github.com>
2021-03-23 17:34:14 +0200

    added redirect to the forms

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -16,6 +16,10 @@ export function AuthProvider( {children} ) {
         return auth.createUserWithEmailAndPassword(email, password);
     };
 
+    function login(email, password){
+        return auth.signInWithEmailAndPassword(email, password);
+    };
+
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(user => {
             setCurrentUser(user);
@@ -28,6 +32,7 @@ export function AuthProvider( {children} ) {
     const value = {
         currentUser,
         signup,
+        login
     };
 
     return (
