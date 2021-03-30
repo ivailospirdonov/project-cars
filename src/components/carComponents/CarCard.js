c90b9004c457667ea95fc700a6b737b83e26943b
Ivailo Spirdonov <78851544+ivailospirdonov@users.noreply.github.com>
2021-03-30 23:38:18 +0300

    Made hover effects on car Cards and Images

--- src/components/carComponents/CarCard.js
+++ src/components/carComponents/CarCard.js
@@ -6,9 +6,11 @@ export default function CarCard({ carId, model, year, price, imageUrl }) {
 
     return (
         <>
-            <Link to={`/cars/details/${carId}`} className="col-4">
+            <Link to={`/cars/details/${carId}`} className="col-4 carCard">
                 <Card>
-                    <Card.Img variant="top" src={imageUrl} class="carCardImg" />
+                    <div className="carCardImgWrap">
+                        <Card.Img variant="top" src={imageUrl} class="carCardImg" />
+                    </div>
                     <Card.Body>
                         <Card.Title>{model}</Card.Title>
                         <Card.Text>Year: {year}</Card.Text>
@@ -17,6 +19,31 @@ export default function CarCard({ carId, model, year, price, imageUrl }) {
                 </Card>
             </Link>
             <style jsx>{`
+
+                .carCard:hover .carCardImg{
+                    transition: transform 1s;
+                    transform: translate(-50%, -50%) scale(1.2);
+                }
+
+                .carCardImgWrap{
+                    display: block;
+                    position: relative;
+                    overflow: hidden;
+                    content: '';
+                    padding-top: calc( (9 / 16) * 100%);
+                    border-bottom: 1px solid rgba(0,0,0,.125);
+                }
+
+                .carCardImg{
+                    display: block;
+                    width: 100%;
+                    height: auto;
+                    position: absolute;
+                    transition: transform 1s;
+                    top: 50%;
+                    left: 50%;
+                    transform: translate(-50%, -50%);
+                }
             `}</style>
         </>
     )
